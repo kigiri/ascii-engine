2a7ae6c2039cec52888428d3e45032e52fa5475a
Clement Denis <le.mikmac@gmail.com>
2018-06-01 17:49:30 +0200

    clear addon

---
+++ addon/clear.js
@@ -0,0 +1,50 @@
+export const clear = ({ cache }) => {
+  const { max, row } = cache
+
+  const clearCharLevel = c => {
+    c.color = undefined
+    c.glyph = undefined
+  }
+
+  const clearChar = c => {
+    clearCharLevel(c[0])
+    clearCharLevel(c[1])
+    clearCharLevel(c[2])
+  }
+
+  const clearCache = () => {
+    let i = -1, c
+    while (++i < max) {
+      clearChar(cache[i])
+    }
+  }
+
+  clearCache.char = (x, y) => {
+    const c = cache[y*row+x]
+    if (!c) return
+    clearChar(c)
+  }
+
+  clearCache.char.
+
+  clearCache.line = y => {
+    const r = y*row
+    const end = r+row
+    if (end > max) return
+    let i = r - 1
+    while (++i < end) {
+      clearChar(cache[i])
+    }
+  }
+
+  clearCache.line.vertical = x => {
+    if (x > row) return
+    let i = 0
+    while ((i < end) {
+      clearChar(cache[i])
+      i += row
+    }
+  }
+
+  return { clear: clearCache }
+}
