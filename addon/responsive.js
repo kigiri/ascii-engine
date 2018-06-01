4adbec058dabf90631085bb6ee4426b3c58ea379
Clement Denis <le.mikmac@gmail.com>
2018-06-01 22:37:49 +0200

    add size cache for responsive mode

--- addon/responsive.js
+++ addon/responsive.js
@@ -1,29 +1,38 @@
 export const dependencies = [ 'loop' ]
 import { debounce } from '../helper/ev.js'
-export const responsive = ({ loop, into, canvas }) => {
-  const [ main, sub ] = canvas.width > canvas.height
-    ? [ 'width', 'height' ]
-    : [ 'height', 'width' ]
+export const responsive = ({ loop, into, canvas, cache: { row, col } }) => {
+  const [ [ main, mainCount ], [ sub, subCount ] ] = canvas.width > canvas.height
+    ? [ [ 'width', row ], [ 'height', col ] ]
+    : [ [ 'height', col ], [ 'width', row ] ]
+
+  console.log({ row, col, main, sub, mainCount, subCount })
 
   const ratio = canvas[sub] / canvas[main]
 
-  let rect = {}
-  const resize = () => {
-    const r = into.getBoundingClientRect()
-    if (rect.width === r.width && rect.height === r.height) return
-    rect = r
-    const size = Math.min(rect.height, rect.width)
+  const size = Object.create(null)
+  const px = size.px = Object.create(null)
+  size.width = null
+  size.height = null
+  size.rect = {}
+  const resize = loop.after.requester(() => {
+    const { height, width } = into.getBoundingClientRect()
+    size.rect = canvas.getBoundingClientRect()
+    const max = Math.min(height, width)
+    if (max === px[main]) return
+    px[main] = max
+    px[sub] = max * ratio
+    size[main] = max / mainCount
+    size[sub] = px[sub] / subCount
+    requestUpdate()
+  })
 
-    loop.before(() => {
-      canvas.style[main] = `${size}px`
-      canvas.style[sub] = `${size * ratio}px`
-    })
-  }
+  window.addEventListener('resize', resize)
 
-  window.addEventListener('resize', debounce(loop.after.requester(resize)))
+  const requestUpdate = debounce(loop.before.requester(() => {
+    canvas.style[main] = `${px[main]}px`
+    canvas.style[sub] = `${px[sub]}px`
+    resize()
+  }))
 
-  return {
-    get realWidth() { return rect.width },
-    get realHeight() { return rect.height },
-  }
+  return { size }
 }
