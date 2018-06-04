49f6c0f770bc4cc582d13a22f388fc9c92aa6993
Clement Denis <le.mikmac@gmail.com>
2018-06-04 09:39:56 +0200

    fix responsive

--- addon/responsive.js
+++ addon/responsive.js
@@ -1,11 +1,8 @@
-export const dependencies = [ 'loop' ]
 import { debounce } from '../helper/ev.js'
-export const responsive = ({ loop, into, canvas, cache: { row, col } }) => {
-  const [ [ main, mainCount ], [ sub, subCount ] ] = canvas.width > canvas.height
-    ? [ [ 'width', row ], [ 'height', col ] ]
-    : [ [ 'height', col ], [ 'width', row ] ]
 
-  const ratio = canvas[sub] / canvas[main]
+export const dependencies = [ 'loop' ]
+export const responsive = ({ loop, into, canvas, cache: { row, col } }) => {
+  const ratio = canvas.width / canvas.height
   const size = Object.create(null)
   const px = size.px = Object.create(null)
   size.width = null
@@ -14,20 +11,18 @@ export const responsive = ({ loop, into, canvas, cache: { row, col } }) => {
   const resize = loop.after.requester(() => {
     const { height, width } = into.getBoundingClientRect()
     size.rect = canvas.getBoundingClientRect()
-    const max = Math.min(height, width)
-    if (max === px[main]) return
-    px[main] = max
-    px[sub] = max * ratio
-    size[main] = max / mainCount
-    size[sub] = px[sub] / subCount
+    px.width = width
+    px.height = height
+    size.width = size.rect.width / row
+    size.height = size.rect.height / col
     requestUpdate()
   })
 
   window.addEventListener('resize', resize)
 
   const requestUpdate = debounce(loop.before.requester(() => {
-    canvas.style[main] = `${px[main]}px`
-    canvas.style[sub] = `${px[sub]}px`
+    canvas.style.maxHeight = `${px.height}px`
+    canvas.style.maxWidth = `${px.width}px`
     resize()
   }))
 
